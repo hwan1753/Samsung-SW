84088724dff978032908c4178c248535e4fc6b98
hawn1753 <john175258@gmail.com>
2020-04-01 00:27:42 +0900

    D3 5642 [Time error2]

--- D3/Professional Sum.py
+++ D3/Professional Sum.py
@@ -2,10 +2,20 @@ T = int(input())
 for test_case in range(1,1+T):
     N = int(input())
     i = list(map(int,input().split()))
-    result = 0
+    result = []
     for num in range(len(i)):
-        for a in range(num,len(i)+1):
-            chk = sum(i[num:a])
-            if result < chk:
-                result = chk
-    print("#{} {}".format(test_case,result))
+        stop = False
+        idx, chk = 0, -9999
+        array_chk = []
+        while (stop == False):
+            if sum(i[num:num+idx+1]) >= 0 and num + idx + 1 <= len(i):
+                chk = sum(i[num:num+idx+1])
+                array_chk.append(chk)
+                idx += 1
+            else:
+                if array_chk != []:
+                    chk = max(array_chk)
+                stop = True
+        result.append(chk)
+    result.append(max(i))
+    print("#{} {}".format(test_case,max(result)))
