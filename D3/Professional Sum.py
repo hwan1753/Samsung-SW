278cdbe709710bfc0058c7b419c50dcbc83dd71b
hawn1753 <john175258@gmail.com>
2020-04-02 14:15:48 +0900

    D3 5642 [PASS]

--- D3/Professional Sum.py
+++ D3/Professional Sum.py
@@ -2,20 +2,15 @@ T = int(input())
 for test_case in range(1,1+T):
     N = int(input())
     i = list(map(int,input().split()))
-    result = []
-    for num in range(len(i)):
-        stop = False
-        idx, chk = 0, -9999
-        array_chk = []
-        while (stop == False):
-            if sum(i[num:num+idx+1]) >= 0 and num + idx + 1 <= len(i):
-                chk = sum(i[num:num+idx+1])
-                array_chk.append(chk)
-                idx += 1
-            else:
-                if array_chk != []:
-                    chk = max(array_chk)
-                stop = True
-        result.append(chk)
-    result.append(max(i))
-    print("#{} {}".format(test_case,max(result)))
+    result = -999
+    a = 0
+    chk = 0
+    while (a < len(i)):
+        while (a < len(i)):
+            chk += i[a]
+            if chk > result:
+                result = chk
+            if chk < 0:
+                chk = 0
+            a += 1
+    print("#{} {}".format(test_case,result))
