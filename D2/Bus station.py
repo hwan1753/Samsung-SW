93dd2f6f28d7eca67e43e9630137fab3a8315386
hawn1753 <john175258@gmail.com>
2020-04-05 19:28:55 +0900

    D3 6485 [PASS]

--- D2/Bus station.py
+++ D2/Bus station.py
@@ -3,19 +3,20 @@ for test_case in range(1,1+T):
     N = int(input())
     array_station = []
     array_count = []
+    for a in range(5000):
+        array_count.append(0)
     route = []
     for aa in range(N):
         route.append(list(map(int,input().split())))
     P = int(input())
     for bb in range(P):
         array_station.append(int(input()))
-        array_count.append(0)
 
     for a in route:
-        A, B = a[0], a[1]
-        for aa in range(array_station.index(A),array_station.index(B)+1):
+        A, B = a[0]-1, a[1]-1
+        for aa in range(A,B+1):
             array_count[aa] += 1
     print("#{}".format(test_case),end=' ')
-    for a in array_count:
-        print(a,end=' ')
+    for a in array_station:
+        print(array_count[a-1],end=' ')
     print()
