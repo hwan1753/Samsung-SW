c1a0fc3501af649e2e6046ce3109952aa854f040
hawn1753 <john175258@gmail.com>
2020-04-08 01:36:26 +0900

    D3 6057 [PASS]

--- D2/Graph Triangle.py
+++ D2/Graph Triangle.py
@@ -1,25 +1,16 @@
 T = int(input())
 for test_case in range(1,1+T):
-    i = list(map(int,input().split()))
-    N, M = i[0], i[1]
-    array_line = []
-    for a in range(M):
-        array_line.append(list(map(int, input().split())))
-    array_line = sorted(array_line)
+    N, M = map(int,input().split())
+    tri = []
     count = 0
-    for idx in range(len(array_line)):
-        stop = False
-        chk = idx
-        while (stop == False):
-            if chk < len(array_line) -1:
-                chk += 1
-            else:
-                break
-            a = array_line[idx]
-            b = array_line[chk]
-            if a[0] == b[0]:
-                if [a[1],b[1]] in array_line:
+    for a in range(N):
+        tri.append([0 * a for a in range(N)])
+    for a in range(M):
+        x, y = map(int,input().split())
+        tri[x-1][y-1] = tri[y-1][x-1] = 1
+    for i in range(N):
+        for j in range(i+1,N):
+            for k in range(j+1,N):
+                if tri[i][j] and tri[i][k] and tri[j][k]:
                     count += 1
-            else:
-                break
     print("#{} {}".format(test_case,count))
