26902a8f320c2080a6ba13610049268686175bf8
hawn1753 <john175258@gmail.com>
2020-04-12 19:24:44 +0900

    D3 3304 [PASS]

--- D2/LCS.py
+++ D2/LCS.py
@@ -1,37 +1,22 @@
 T = int(input())
 for test_case in range(1,1+T):
-    a, b = input().split(' ')
-    result_b = ''
-    idx_a = 0
-    for idx_b in range(len(b)):
-        stop = False
-        while (stop==False):
-            if b[idx_b] == a[idx_a]:
-                chk = idx_a
-                result_b += b[idx_b]
-                break
+    i = list(map(str, input().split(' ')))
+    A = list(map(str, i[0]))
+    B = list(map(str, i[1]))
+    A.insert(0,'-')
+    B.insert(0,'-')
+    array_result = []
+    for x in range(len(B)):
+        array_result.append([])
+    for b in range(len(B)):
+        for a in range(len(A)):
+            if A[a] == '-' or B[b] == '-':
+                array_result[b].append(0)
+            elif A[a] == B[b]:
+                array_result[b].append(array_result[b-1][a-1]+1)
             else:
-                if idx_a != len(a)-1:
-                    idx_a += 1
+                if array_result[b][a-1] > array_result[b-1][a]:
+                    array_result[b].append(array_result[b][a-1])
                 else:
-                    stop = True
-                    idx_a = chk
-    idx_b = 0
-    result_a = ''
-    for idx_a in range(len(a)):
-        stop = False
-        while (stop==False):
-            if a[idx_a] == b[idx_b]:
-                chk = idx_b
-                result_a += a[idx_a]
-                break
-            else:
-                if idx_b != len(a)-1:
-                    idx_b += 1
-                else:
-                    stop = True
-                    idx_b = chk
-    if len(result_a) > len(result_b):
-        print("#{} {}".format(test_case,len(result_a)))
-    else:
-        print("#{} {}".format(test_case, len(result_b)))
+                    array_result[b].append(array_result[b-1][a])
+    print("#{} {}".format(test_case,array_result[len(B)-1][len(A)-1]))
