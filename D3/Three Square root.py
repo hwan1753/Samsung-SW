5e53bb3acf512a6012abc9a170c837aa0c852c26
hawn1753 <john175258@gmail.com>
2020-04-14 01:03:00 +0900

    D3 5688 [PASS]

--- D3/Three Square root.py
+++ D3/Three Square root.py
@@ -2,11 +2,17 @@ result_array = []
 T = int(input())
 for test_case in range(1,1+T):
     N = int(input())
-
-    if round(N ** (1.0/3.0),10) / int(round(N ** (1.0/3.0),10)) == 1:
-        result_array.append(int(round(N ** (1.0/3.0),10)))
-    else:
-        result_array.append(-1)
+    a = 1
+    while True:
+        chk = a * a * a
+        if N > chk:
+            a += 1
+        elif N == chk:
+            result_array.append(a)
+            break
+        else:
+            result_array.append(-1)
+            break
 
 for res in range(len(result_array)):
     print("#{} {}".format(res+1, result_array[res]))
