7732ee0a76a44229d7436f322327f6c1fa5b7265
hawn1753 <john175258@gmail.com>
2020-04-21 15:15:17 +0900

    D3 5293 [PASS]

--- D3/Binary string.py
+++ D3/Binary string.py
@@ -2,53 +2,24 @@ res = []
 T = int(input())
 for test_case in range(1,1+T):
     A, B, C, D = map(int,input().split()) # 00 01 10 11
-    size = A+B+C+D
-    # snt = ['-' for _ in range(size+1)]
-    top = 0
-    stop = False
-    if C == 0 and B == 0:
+    if B == 0 and C == 0:
+        if D == 0 and A != 0:
+            snt = '0' * A + '0'
+        elif A == 0 and D != 0:
+            snt = '1' * D + '1'
+        else:
+            snt = 'impossible'
+    elif B - C > 1 or C - B > 1:
         snt = 'impossible'
-    elif B > C:
-        top = A + 1
-        snt = '0' * A + '1'* D + '1'
-        A = 0
-        B -= 1
-        # for b in range(D+1):
-        top += 1
-        while stop==False:
-            if snt[size] != '-':
-                break
-            if snt[top-1] == '0' and B != 0:
-                B -= 1
-                snt[top] = '1'
-                top += 1
-            elif snt[top-1] == '1' and C != 0:
-                C -= 1
-                snt[top] = '0'
-                top += 1
+    elif B - C == 1:
+        snt = '0' * A + '01' * B + '1' * D
+    elif C - B == 1:
+        snt = '1' * D + '10' * C + '0' * A
     else:
-        top = D + 1
-        for a in range(D + 1):
-            snt[a] = '1'
-            D = 0
-        C -= 1
-        for b in range(A+1):
-            snt[top] = '0'
-            top += 1
-        while stop == False:
-            if snt[size] != '-':
-                break
-            if snt[top - 1] == '0' and B != 0:
-                B -= 1
-                snt[top] = '1'
-                top += 1
-            elif snt[top - 1] == '1' and C != 0:
-                C -= 1
-                snt[top] = '0'
-                top += 1
+        snt = '0' * A + '01' * B + '1' * D + '0'
     res.append(snt)
-# for a in range(len(res)):
-#     print("#{} ".format(a+1), end='')
-#     for b in res[a]:
-#         print(b, end='')
-#     print()
+for a in range(len(res)):
+    print("#{} ".format(a+1), end='')
+    for b in res[a]:
+        print(b, end='')
+    print()
